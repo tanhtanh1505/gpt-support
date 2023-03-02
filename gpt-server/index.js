2a4665c6e7b257e8b09b92a82aec6f8551ba6157
tanhtanh1505 <tanhtanh1505@gmail.com>
2023-03-03 05:24:04 +0700

    feat: GPT response

--- gpt-server/index.js
+++ gpt-server/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 require("dotenv").config();
+const { getSolution } = require("./services/chatgpt");
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", async (req, res) => {
+  const text = req.query.text;
+  const solution = await getSolution(text);
+
+  res.send(solution);
+});
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ gpt-server/services/chatgpt.js
@@ -0,0 +1,16 @@
+const { Configuration, OpenAIApi } = require("openai");
+require("dotenv").config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+module.exports.getSolution = async (text) => {
+  const completion = await openai.createCompletion({
+    model: "text-davinci-002",
+    prompt: text,
+    max_tokens: 2048,
+  });
+  return completion.data.choices[0].text;
+};
