3b9a26269144874f0e8c6d8cd2d27334f51226ba
tanhtanh1505 <tanhtanh1505@gmail.com>
2023-03-06 19:59:48 +0700

    feat: Script build

---
+++ client/scripts/build.sh
@@ -0,0 +1,5 @@
+npm run build
+if [ -f gpt-support.zip ]; then
+    rm gpt-support.zip
+fi
+zip -r gpt-support.zip build/
---
+++ client/scripts/move_to_server.sh
@@ -0,0 +1,2 @@
+cd ../../../roofy
+scp -i llele-server.pem ../extension/gpt/client/gpt-support.zip ubuntu@ec2-54-169-39-45.ap-southeast-1.compute.amazonaws.com:~
